cfcd0b67c7f1afeabb06617ba96ddd2782f52381
JRafaelLobo <jose.lobo@unitec.edu>
2023-11-12 13:43:03 -0600

    Tengo que seguir con mi parte

--- src/standard/file/manager/Campo.java
+++ src/standard/file/manager/Campo.java
@@ -1,19 +1,62 @@
 package standard.file.manager;
+
 public class Campo {
-    public String Nombre;
-    public int Tamano;
-    public Object Tipo;
+
+    private String Nombre;
+    private int Tamano, Tipo;
+
     /*tipos
     1:int
-    2:String
-    3:double o float
-    4:caracter
-    */
-
-    public Campo(String Nombre, int Tamano, Object Tipo) {
+    2:double o float
+    3:caracter
+     */
+    public Campo(String Nombre, int Tamano, int Tipo) {
         this.Nombre = Nombre;
         this.Tamano = Tamano;
         this.Tipo = Tipo;
     }
-    
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public int getTamano() {
+        return Tamano;
+    }
+
+    public void setTamano(int Tamano) {
+        this.Tamano = Tamano;
+    }
+
+    public Object getTipo() {
+        return Tipo;
+    }
+
+    public void setTipo(int Tipo) {
+        this.Tipo = Tipo;
+    }
+
+    @Override
+    public String toString() {
+        String tipo;
+        switch (Tipo) {
+            case 1:
+                tipo = "int";
+                break;
+            case 2:
+                tipo = "Double";
+                break;
+            case 3:
+                tipo = "char";
+                break;
+            default:
+                tipo="Unknown";
+        }
+        return Nombre + ": " + tipo +"["+Tamano+"]";
+    }
+
 }
